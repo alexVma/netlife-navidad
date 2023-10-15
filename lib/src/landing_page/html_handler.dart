10865140431d7553cfdee797e42486be3686044e
Alex V <aminga@netlife.net.ec>
2023-10-14 22:45:58 -0500

    test2

--- lib/src/landing_page/html_handler.dart
+++ lib/src/landing_page/html_handler.dart
@@ -16,13 +16,15 @@ class _HTMLWidgetState extends State<HTMLWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return Scaffold(
+      body: Text('hola\nhola\nhola\nhola\nhola\nhola\nhola\nhola\nhola\nhola\n'),
+    );/*const SizedBox(
       width: double.maxFinite, // Define el ancho del iframe
       height: double.maxFinite, // Define la altura del iframe
       child: AspectRatio(
         aspectRatio: 1,
         child: HtmlElementView(viewType: 'hello-html'),
       ),
-    );
+    );*/
   }
 }
