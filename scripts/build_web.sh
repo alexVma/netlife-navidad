7e06015c770f3ac3309f63b6fd480f7cac502f2e
Alex V <aminga@netlife.net.ec>
2023-10-14 22:00:05 -0500

    ci/cd 1

---
+++ scripts/build_web.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu pipefail
+
+rm -rf build/web dist
+mkdir dist
+
+flutter pub run build_runner build
+
+BUILD_NUM=$(git rev-list --count HEAD)
+echo "Num Commits: $BUILD_NUM"
+BUILD_NUM=$((BUILD_NUM + 1000000))
+echo "Build Number: $BUILD_NUM"
+
+# Build Web App
+flutter build web --base-href /netlife-navidad/web/
+
+jq '. + {build_number:$buildNumber}' --arg buildNumber "$BUILD_NUM" build/web/version.json >version.json
+mv version.json build/web/
+mv build/web dist/web
+
+echo "{\"buildNum\":$BUILD_NUM, \"dt\": \"$(date --iso-8601=seconds)\"}" >dist/build
+cat dist/build
